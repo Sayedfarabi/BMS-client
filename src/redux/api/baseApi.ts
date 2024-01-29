007afaa8302f0f5187eb0a2f771c22d87891c49e
ABU SAYED <farabisayed6099@gmail.com>
2024-01-30 02:31:29 +0600

    base api customize

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,37 @@
+// import { useAppDispatch } from "./../hooks";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+// import { logout } from "../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
+
+// // customs base query
+// const baseQueryWithAccessToken = async (args, api, extraOptions) => {
+//   const result = await baseQuery(args, api, extraOptions);
+//   // console.log(result);
+
+//   if (result.error?.status === 401) {
+//     api.dispatch(logout());
+
+//     // result = await baseQuery(args, api, extraOptions);
+//   }
+//   return result;
+// };
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include",
-  }),
+  baseQuery: baseQuery,
+  // baseQuery: baseQueryWithAccessToken,
   endpoints: () => ({}),
 });
